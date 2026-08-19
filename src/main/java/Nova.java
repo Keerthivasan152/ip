07484672d80d640886bc765ee4bd46e9dc07fb6d
Keerthivasan512 <e1526960@u.nus.edu>
2026-08-20 07:25:10 +0800

    Level-2: Add and list tasks

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -9,14 +9,23 @@ public class Nova {
 
         System.out.println(banner);
         System.out.println("Hello! I'm Nova. What can I do for you?");
+        String[] tasks  = new String[100];
+        int taskCount = 0;
         Scanner scanner = new Scanner(System.in);
         while(true) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("added: " + input);
             }
-            System.out.println(input);
         }
 
 
