3fb5ce06264ad978efeff041bf62a796addb1baa
Keerthivasan512 <e1526960@u.nus.edu>
2026-08-20 07:11:50 +0800

    Level-1: Echo input, exit on bye

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Nova {
     public static void main(String[] args) {
         String banner = " _   _\n"
@@ -8,6 +9,16 @@ public class Nova {
 
         System.out.println(banner);
         System.out.println("Hello! I'm Nova. What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(input);
+        }
+
+
     }
 }
