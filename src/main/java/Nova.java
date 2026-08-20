d05d5e076dd45905c439ffee46b9b99027a41594
Keerthivasan512 <e1526960@u.nus.edu>
2026-08-20 08:03:57 +0800

    Level-3: Mark tasks as done and unmark

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -9,20 +9,40 @@ public class Nova {
 
         System.out.println(banner);
         System.out.println("Hello! I'm Nova. What can I do for you?");
-        String[] tasks  = new String[100];
+        Task[] tasks  = new Task[100];
         int taskCount = 0;
         Scanner scanner = new Scanner(System.in);
         while(true) {
             String input = scanner.nextLine();
-            if (input.equals("bye")) {
+            String[] parts = input.split(" ", 2);
+            String command = parts[0];
+            if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else if (input.equals("list")) {
+            } else if (command.equals("list")) {
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    String status = tasks[i].isDone() ? "[X]" : "[ ]";
+                    System.out.println((i + 1) + "." + status + " " + tasks[i].getDescription());
+                }
+
+            } else if (command.equals("mark")) {
+                if (parts.length == 2) {
+                    int index = Integer.parseInt(parts[1]);
+                    System.out.println("Nice! I've marked this task as done: ");
+                    tasks[index - 1].markDone();
+                    String status = tasks[index - 1].isDone() ? "[X]" : "[ ]";
+                    System.out.println(status + " " + tasks[index - 1].getDescription());
+                }
+            } else if (command.equals("unmark")) {
+                if (parts.length == 2) {
+                    int index = Integer.parseInt(parts[1]);
+                    System.out.println("Ok, I've marked this task as not done yet: ");
+                    tasks[index - 1].markUndone();
+                    String status = tasks[index - 1].isDone() ? "[X]" : "[ ]";
+                    System.out.println(status + " " + tasks[index - 1].getDescription());
                 }
             } else {
-                tasks[taskCount] = input;
+                tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println("added: " + input);
             }
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public boolean isDone() {
+        return this.isDone;
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void markUndone() {
+        this.isDone = false;
+    }
+
+}
